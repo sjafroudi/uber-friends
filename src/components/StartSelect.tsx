35f8e284dbc98c9da62ac7747eda131936b72071
sara jafroudi <sjafroudi5@gmail.com>
2023-05-05 15:02:36 -0700

    added craco and fixed indendation

--- src/components/StartSelect.tsx
+++ src/components/StartSelect.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Button from "react-bootstrap/Button";
 
 const StartSelect = () => {
-    return (
-        <div className="main-frame-container d-flex align-items-center justify-content-center vh-100">
-        <div className="container">
-          <h1 className="text-center pb-5">uber-friends</h1>
-            <div className="row row-cols-1 row-cols-md-2 g-3">
-                <div className="col">
-                    <Button variant="primary" type="submit" className="w-100">
-                        </Button>
-
-                </div>
-                </div> 
-                </div>
-                </div>
-    );
-    };
-    export default StartSelect;
+  return (
+    <div className="main-frame-container d-flex align-items-center justify-content-center vh-100">
+      <div className="container">
+        <h1 className="text-center pb-5">uber-friends</h1>
+        <div className="row row-cols-1 row-cols-md-2 g-3">
+          <div className="col">
+            <Button variant="primary" type="submit" className="w-100"></Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default StartSelect;
